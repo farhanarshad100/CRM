724c55e5bf3c5bf18151b0c59bb45341f64af226
Farhan <farhanarshad100@gmail.com>
2026-06-02 12:00:18 +0530

    UI change

---
+++ src/app/(main)/attendance/page.js
@@ -0,0 +1,14 @@
+import DashboardLeft from '@/components/dashboardSection/dashboardLeft'
+import DashboardRight from '@/components/dashboardSection/dashboardRight'
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Attendance = () => {
+  return (
+<>
+<h1>Attendance Log</h1>
+</>
+  )
+}
+
+export default Attendance
