63dd84a28a82eb9d998907b1a7a2504543f0adf7
Farhan <farhanarshad100@gmail.com>
2026-06-18 21:03:10 +0530

    Card update

--- src/components/holiday/holidayTop.js
+++ src/components/holiday/holidayTop.js
@@ -2,6 +2,7 @@ import { Box, Grid, Typography } from '@mui/material'
 import React from 'react'
 import style from './style'
 import Image from 'next/image'
+import Button from '../ui/Button'
 
 const HolidayTop = () => {
   return (
@@ -9,7 +10,7 @@ const HolidayTop = () => {
     <Grid container spacing={3}>
         <Grid size={4}>
             <Box sx={style.holidayCard}>
-                <Box>
+                <Box sx={style.holiDetails}>
                     <Typography variant='body1'>Total Holidays this Month</Typography>
                     <Typography variant='h3'>04</Typography>
                 </Box>
@@ -21,8 +22,29 @@ const HolidayTop = () => {
                 </Box>
             </Box>
         </Grid>
-         <Grid size={4}>one</Grid>
-          <Grid size={4}>one</Grid>
+     <Grid size={4}>
+            <Box sx={style.holidayCard}>
+                <Box sx={style.holiDetails}>
+                    <Typography variant='body1'>Upcoming Mandatory</Typography>
+                    <Typography variant='h3' style={{color:"#A12D01"}}>02</Typography>
+                </Box>
+                <Box sx={style.iconSection}>
+                    <Image src="/images/question2.svg" alt='Upcoming'
+                    width={5}
+                    height={22}
+                    />
+                </Box>
+            </Box>
+        </Grid>
+  <Grid size={4}>
+            <Box sx={{...style.holidayCard, ...style.nextHolidayCard}}>
+                <Box sx={style.holiDetails}>
+                    <Typography variant='body1'>Upcoming Mandatory</Typography>
+                    <Typography variant='h3'>03 Days</Typography>
+                </Box>
+          <Button sx={{background:"rgb(255 255 255 / 20%)", padding:"8px 16px", color:"#FFFFFF", fontSize:"14px", fontWeight:"800"}}>View List</Button>
+            </Box>
+        </Grid>
     </Grid>
 </Box>
   )
--- src/components/holiday/style.js
+++ src/components/holiday/style.js
@@ -7,11 +7,42 @@ borderRadius: "8px",
 padding:"24px",
 display:"flex",
 justifyContent:"space-between",
+alignItems:"center",
+
 },
 iconSection:{
-    background:"#004CCA",
+    background:"rgb(0 76 202 / 10%)",
     padding:"12px",
     borderRadius:"8px",
+},
+holiDetails:{
+"& p":{
+    fontSize:"14px",
+    color:"#424656",
+    fontWeight:"500",
+    lineheight:"20px",
+},
+"& h3":{
+       fontSize:"36px",
+    color:"#004CCA",
+    fontWeight:"800",
+    lineheight:"40px",
+}
+},
+nextHolidayCard:{
+    background:"linear-gradient(135deg, #004CCA 0%, #0062FF 100%)",
+    "& p":{
+    fontSize:"14px",
+    color:"#fff",
+    fontWeight:"500",
+    lineheight:"20px",
+},
+"& h3":{
+       fontSize:"36px",
+    color:"#fff",
+    fontWeight:"800",
+    lineheight:"40px",
+}
 }
 
 }
